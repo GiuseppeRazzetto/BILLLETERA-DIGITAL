c2715a32435ffb2f7a41b1c69c9541201c33e65a
RGiuseppeZ <giusepperazzeto@gmail.com>
2024-12-01 23:57:47 -0500

    Update database configuration for production

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'localhost';
-$dbname = 'digital_wallet';
-$username_db = 'root';
-$password_db = '';
+$host = getenv('DB_HOST') ?: 'sql.freedb.tech';
+$dbname = getenv('DB_NAME') ?: '[tu_nombre_base_datos]';
+$username_db = getenv('DB_USER') ?: '[tu_usuario]';
+$password_db = getenv('DB_PASSWORD') ?: '[tu_contraseña]';
