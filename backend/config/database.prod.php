c6e2bda6719e3d8d15357e833e09d35e6fec9332
RGiuseppeZ <giusepperazzeto@gmail.com>
2024-12-02 06:17:37 -0500

    Update database production credentials

---
+++ backend/config/database.prod.php
@@ -0,0 +1,5 @@
+<?php
+$host = 'sql.freedb.tech';
+$dbname = 'freedb_digital_wallet2';  // Usa el nombre que elegiste
+$username_db = 'freedb_giuseppe';  // Usa el usuario que elegiste
+$password_db = 'Kh4M%*wJ2Nq4xE@';  // La contraseña que elegiste
